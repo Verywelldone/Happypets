ee3e98fbc96978fd343a9ad253a11ee988cfe152
BogdanFatu <ionutbogdan11@gmail.com>
2019-03-11 15:26:36 +0200

    Primul pas spre data handle

--- src/Controllers/Register.js
+++ src/Controllers/Register.js
@@ -5,9 +5,24 @@ import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap'
 
 class Register extends Component {
     constructor(){
-           super();
+        super();
+        this.state = {
+            name: '',
+            address:'',
+            email:'',
+            password:''
+
+        }
+          
            this.createAcount = this.createAcount.bind(this); // initializare paht
        }
+
+
+       /* DATA HANDLE */
+       handleChange (event) {
+        this.setState({[event.target.name]: event.target.value} )
+        console.log(this.state.password + " PASSWORD");
+      }
 
        /*
                         CONEXIUNE INTRE PAGINI 
@@ -21,31 +36,34 @@ class Register extends Component {
 
         return(
             
+            //  onChange din inputs parseaza numele inputului in state-ul din constructor prin functia handleChange
+            //  functia handleChange, in functie de parametrul primit ( numele inputului ) alege ce obiect din state trebuie modificat
+            
             <div id="primaPagina">
             <Col><h2>Register for a Happy Puppy</h2></Col>
                 <Form>
                     <Col>
                         <FormGroup>
                             <Label for="name">Your name:</Label>
-                            <Input type="text" placeholder = "First and last name" name="name" id=""/>
+                            <Input type="text" placeholder = "First and last name" name="name" onChange={event => this.handleChange(event)} id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup>
                             <Label for="address">Your address:</Label>
-                            <Input type="text" placeholder = "Address here" name="name" id=""/>
+                            <Input type="text" placeholder = "Address here" onChange={event => this.handleChange(event)} name="name" id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup>
                             <Label for="email">Email:</Label>
-                            <Input type="email" placeholder = "email@email.com" name="email" id=""/>
+                            <Input type="email" placeholder = "email@email.com" onChange={event => this.handleChange(event)} name="email" id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup>
                             <Label for="password">Create a password:</Label>
-                            <Input type="password" name="password" id=""/>
+                            <Input type="password" name="password" onChange={event => this.handleChange(event)} id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
