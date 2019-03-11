45f062e66168e101a4f4152012032a0c5ad07e4a
BogdanFatu <ionutbogdan11@gmail.com>
2019-03-11 17:17:03 +0200

    Sign up preia toate datele din form simultan si le afiseaza in consola. Urmeaza manevrarea lor

--- src/Controllers/Register.js
+++ src/Controllers/Register.js
@@ -19,16 +19,41 @@ class Register extends Component {
 
 
        /* DATA HANDLE */
+            // functia de mai jos ( handleChange ) se ocupa de TOATE datele din input simultan.
        handleChange (event) {
         this.setState({[event.target.name]: event.target.value} )
-        console.log(this.state.password + " PASSWORD");
       }
+        /* cele 4 functii de mai jos, se ocupa fiecare de cate un singur input */
+        handleName( name ){
+            this.setState({name: name.target.value});
+            // console.log(this.state.name + " AICI E NUMELE")
+        }
+
+        handleAddres(adr){
+            this.setState({address: adr.target.value});
+            // console.log(this.state.name + " AICI E Adresa")
+        }
+
+        handleEmail(email){
+            this.setState({email:email.target.value});
+            // console.log(this.state.email + " AICI E emailul")
+        }
+        handlePassword(pass){
+            this.setState({password:pass.target.value});
+            // console.log(this.state.password + " AICI E Password")
+        }
 
        /*
                         CONEXIUNE INTRE PAGINI 
        */
        createAcount(){
-           const  path ="/";   // Path-ul 
+          console.log(
+              this.state.name + " Aici e numele \n" + 
+              this.state.email + " Aici e emailul \n"+
+              this.state.address + " Aici e adresa \n"+
+              this.state.password + " Aici e parola \n"
+          )
+                const  path ="/";   // Path-ul 
            this.props.history.push(path);  /// redirect catre path
        }
     
@@ -45,25 +70,25 @@ class Register extends Component {
                     <Col>
                         <FormGroup>
                             <Label for="name">Your name:</Label>
-                            <Input type="text" placeholder = "First and last name" name="name" onChange={event => this.handleChange(event)} id=""/>
+                            <Input type="text" placeholder = "First and last name" name="name" onChange={event => this.handleName(event)} id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup>
                             <Label for="address">Your address:</Label>
-                            <Input type="text" placeholder = "Address here" onChange={event => this.handleChange(event)} name="name" id=""/>
+                            <Input type="text" placeholder = "Address here" onChange={event => this.handleAddres(event)} name="name" id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup>
                             <Label for="email">Email:</Label>
-                            <Input type="email" placeholder = "email@email.com" onChange={event => this.handleChange(event)} name="email" id=""/>
+                            <Input type="email" placeholder = "email@email.com" onChange={event => this.handleEmail(event)} name="email" id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup>
                             <Label for="password">Create a password:</Label>
-                            <Input type="password" name="password" onChange={event => this.handleChange(event)} id=""/>
+                            <Input type="password" name="password" onChange={event => this.handlePassword(event)} id=""/>
                         </FormGroup>
                     </Col>
                     <Col>
