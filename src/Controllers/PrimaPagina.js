fe644a2244bcb952924b9a91fe6007bf4826464a
Natalia Pivoda <pivoda.natalia@yahoo.com>
2019-03-12 19:43:17 +0200

    am stricat paginile

--- src/Controllers/PrimaPagina.js
+++ src/Controllers/PrimaPagina.js
@@ -1,23 +1,24 @@
 import React, {Component} from  "react";
-import { Button } from "reactstrap";
+// import { Button } from "reactstrap";
 import "./LoginCSS.css"
+import Card from "./Card"
 
 
 class PrimaPagina extends Component {
-    constructor(){
-        super();
-        this.hostPage = this.hostPage.bind(this);
-        this.clientPage = this.clientPage.bind(this);
-    }
-    hostPage(){
-        const path ="/hostPage";
-        this.props.history.push(path);  
-    }
+    // constructor(){
+    //     super();
+    //     this.hostPage = this.hostPage.bind(this);
+    //     this.clientPage = this.clientPage.bind(this);
+    // }
+    // hostPage(){
+    //     const path ="/hostPage";
+    //     this.props.history.push(path);  
+    // }
     
-    clientPage(){
-        const path ="/clientPage";
-        this.props.history.push(path);  
-    }
+    // clientPage(){
+    //     const path ="/clientPage";
+    //     this.props.history.push(path);  
+    // }
 
     render(){
 
@@ -25,11 +26,11 @@ class PrimaPagina extends Component {
 
         
         <div id ="primaPagina">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In molestias ea corrupti alias officiis dolore aspernatur id commodi accusamus ex explicabo consequatur, porro quasi. Repudiandae molestias excepturi omnis voluptatum corrupti!</p>
-            <form action="">
+                <Card />
+            {/* <form action="">
                 <Button onClick={this.hostPage}>Host</Button>
                 <Button onClick={this.clientPage}>Client</Button>
-            </form>
+            </form> */}
         </div>
         )
     }
