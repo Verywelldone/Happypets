68d894731741d24637a1476700804638712b087e
BogdanFatu <ionutbogdan11@gmail.com>
2019-03-12 20:02:08 +0200

    asd

--- src/Controllers/Card.js
+++ src/Controllers/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-
+import "./LoginCSS.css"
 
 
 
--- src/Controllers/submitbtn.js
+++ src/Controllers/submitbtn.js
@@ -1,35 +1,35 @@
-import React, {Component} from  "react";
-import React, {Component} from  "reactstrap"
-import React, {Component} from "vue"
-import React, {Component} from "TweenLite"
-import "./submitbtn.css";
+// import React, {Component} from  "react";
+// import React, {Component} from  "reactstrap"
+// import React, {Component} from "vue"
+// import React, {Component} from "TweenLite"
+// import "./submitbtn.css";
 
-class submitbtn extends Component {
-constructor(){
-    new Vue({
-        el: '#submit-button',
-        data: {
-          clicked: false,    
-          loading: false,
-          loaded: false
-        }
+// class submitbtn extends Component {
+// constructor(){
+//     new Vue({
+//         el: '#submit-button',
+//         data: {
+//           clicked: false,    
+//           loading: false,
+//           loaded: false
+//         }
         
-      })
-    }
-}
-render () {
-return(
-methods: {
-    clickedSubmit () {
-      this.clicked = true
-    }
-  }
-  computed: {
-    buttonClass () {
-      if (this.clicked) {
-        return 'round'
-      }
-      return ''
-    }
-  })
-}
+//       })
+//     }
+// }
+// render () {
+// return(
+// methods: {
+//     clickedSubmit () {
+//       this.clicked = true
+//     }
+//   }
+//   computed: {
+//     buttonClass () {
+//       if (this.clicked) {
+//         return 'round'
+//       }
+//       return ''
+//     }
+//   })
+// }
