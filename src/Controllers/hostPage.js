b68019b93a0bf39ef8d1db272654ab8bd45e611e
Marius Vraja <vrajavalentin@gmail.com>
2019-03-14 18:23:24 +0200

    fara buton

--- src/Controllers/hostPage.js
+++ src/Controllers/hostPage.js
@@ -51,17 +51,12 @@ render(){
                 <Button color="primary" onClick={() => this.onRadioBtnClick(3)} active={this.state.rSelected === 3}>3 Zile</Button>
                 <Button color="primary" onClick={() => this.onRadioBtnClick(4)} active={this.state.rSelected === 4}>4 Zile</Button>
                 <Button color="primary" onClick={() => this.onRadioBtnClick(5)} active={this.state.rSelected === 5}>5 Zile</Button>
-            </ButtonGroup>
+                </ButtonGroup>
                 <p>Selecati: {this.state.rSelected} zile</p>
                 </Col>
                 </Row>
                
-                <Button id="submit-button" class="submit-btn" color="success" onClick={alert("Thank you")}>Gazduiestea</Button>
-                <svg class="loader-svg">
-                <path stroke="#CCCCCC" fill="none" stroke-width="4" d="M25,2.5A22.5,22.5 0 1 1 2.5,25A22.5,22.5 0 0 1 25,2.5"></path></svg>
-                <svg class="loader-svg">
-                <path stroke="#20BF7E" fill="none" stroke-width="4" d="M25,2.5A22.5,22.5 0 1 1 2.5,25A22.5,22.5 0 0 1 25,2.5"></path></svg>
-
+                <Button color="success" onClick={alert("Thank you")}>Gazduiestea</Button>
 
             </Form>
        </Container>
