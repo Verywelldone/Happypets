63d11185a9160cd4abdae7bcd6cf8cf3254a4578
Natalia Pivoda <pivoda.natalia@yahoo.com>
2019-03-15 16:13:58 +0200

    le-am decomentat pt ca nu mai functionau celelate pagini :D

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,15 +1,15 @@
 import Login from "../Controllers/Login.js";
 import Register from "../Controllers/Register";
-// import PrimaPagina from "../Controllers/PrimaPagina";
-// import Host from "../Controllers/hostPage";
-// import Client from "../Controllers/clientPage";
+import PrimaPagina from "../Controllers/PrimaPagina";
+import Host from "../Controllers/hostPage";
+import Client from "../Controllers/clientPage";
 
 const routes = [
     { path: '/', component: Login , text:"Contul meu" },    
     { path: '/Register', component: Register , text:"Creeaza cont"},
-    // { path: '/PrimaPagina', component: PrimaPagina, text:"Prima Pagina" },
-    // { path: '/hostPage', component: Host, text:"Host Page" },
-    // { path: '/clientPage', component: Client, text:"Client Page" }
+    { path: '/PrimaPagina', component: PrimaPagina, text:"Prima Pagina" },
+    { path: '/hostPage', component: Host, text:"Host Page" },
+    { path: '/clientPage', component: Client, text:"Client Page" }
 ];
 
 export default routes;
