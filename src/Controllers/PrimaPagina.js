d6ac02a268a6c7ac310c7ff3c2a96b27ce033f97
Natalia Pivoda <pivoda.natalia@yahoo.com>
2019-03-18 23:38:38 +0200

    am adaugat o poza pe pagina main page

--- src/Controllers/PrimaPagina.js
+++ src/Controllers/PrimaPagina.js
@@ -1,13 +1,26 @@
 import React, {Component} from "react"
+import "./main-page.css";
 
   class firstPage extends Component {
     render(){
         return (
-        
-        <div> Actually first page</div>
+          <div id="maindiv"> 
+            <img src="https://www.healthy-pets.co.uk/image/image_site/home-dog-running.jpg" alt="pets" />
+            <p>#HAPPY PUPPY</p>
           
           
-          )
+          
+          
+          
+          
+          
+          
+          
+          
+          </div>
+          
+          
+          );
     }
 }
 
