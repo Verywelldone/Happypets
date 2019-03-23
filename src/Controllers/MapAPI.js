d306247a904e7e7bb0860c62f90a0859e14f79f7
BogdanFatu <ionutbogdan11@gmail.com>
2019-03-23 14:19:50 +0200

    Modificari pe la harta  + am adaugat un fel de PIN pe ea

--- src/Controllers/MapAPI.js
+++ src/Controllers/MapAPI.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+const AnyReactComponent = ({ text }) => (
+  <div style={{
+    color: 'white', 
+    background: 'grey',
+    padding: '15px 10px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '100%',
+    transform: 'translate(-50%, -50%)'
+  }}>
+    {text}
+  </div>
+);
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
       lat: 45.64,
       lng: 25.60
     },
-    zoom: 11
+    zoom: 13
   };
  
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '75vh', width: '100%' }}>
         <GoogleMapReact
         
           defaultCenter={this.props.center}
@@ -23,8 +36,8 @@ class SimpleMap extends Component {
         >
           <AnyReactComponent
             lat={45.64}
-            lng={25.60}
-            text={'Brasov'}
+            lng={25.615}
+            text={'Fatu Bogdan'}
           />
         </GoogleMapReact>
       </div>
