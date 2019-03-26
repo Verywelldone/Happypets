25f74f678ae9676766cf5872aab8805006ab39c8
BogdanFatu <ionutbogdan11@gmail.com>
2019-03-26 18:37:26 +0200

    AVEM HARTAAAAAAA

--- src/Controllers/MapAPI.js
+++ src/Controllers/MapAPI.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
 const AnyReactComponent = ({ text }) => (
+ 
   <div style={{
     color: 'white', 
     background: 'grey',
@@ -17,6 +18,14 @@ const AnyReactComponent = ({ text }) => (
   </div>
 );
 class SimpleMap extends Component {
+ 
+  constructor(props){
+    super(props)
+    this.state ={
+      "key": "AIzaSyCS-nM5E7swe1c9BWgIhipBeqA14kWWkDI"
+    }
+  }
+ 
   static defaultProps = {
     center: {
       lat: 45.64,
@@ -30,15 +39,12 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '75vh', width: '100%' }}>
         <GoogleMapReact
-        
+        bootstrapURLKeys={{ key:'AIzaSyBmFPKtqmN4mC_yOi1v7THSdXWlx4sqtP8'}}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={45.64}
-            lng={25.615}
-            text={'Fatu Bogdan'}
-          />
+          <AnyReactComponent  lat={45.64}   lng={25.615}   text={'Fatu Bogdan'}/>
+          <AnyReactComponent  lat={45.6599}   lng={25.6033}   text={'Paul'}/>
         </GoogleMapReact>
       </div>
     );
