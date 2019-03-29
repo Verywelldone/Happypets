b482649e42834b16ff14c7e4a842826c682b57c2
Marius Vraja <vrajavalentin@gmail.com>
2019-03-29 20:29:30 +0200

    enter

--- src/Controllers/EnterPress.js
+++ src/Controllers/EnterPress.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { keydownScoped } from 'react-keydown';
 
 class EnterPress extends React.Component {
-
-  @keydownScoped('enter') // or specify `which` code directly, in this case 13
-  submit() {
-
+constructor(props) {
+    super(props);
+  this.handleKeyPress = this.handleKeyPress.bind(this);
+}
+handleKeyPress = (event) => {
+  if(this.event.key === 'Enter'){
+    // console.log('enter press here! ');
   }
 }
-export defoult EnterPress;
+}
+export default EnterPress;
